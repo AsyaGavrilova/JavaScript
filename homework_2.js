10fb5a878af78e070eb2f13e4036fff5a09d673d
Asya Gavrilova <agavrilova44kaas@gmail.com>
2022-06-26 10:42:32 +0300

    Update 2 files

--- homework_2.js
+++ homework_2.js
@@ -52,7 +52,7 @@ let age_2 = 18;
 let age_3 = 60;
 
 const checkAge = (age_1) => {
-    if (Number(age_1)) {
+    if (age_1 && !isNaN(age_1)) {
         if (age_1 < age_2) {
             console.log("You don’t have access cause your age is " + age_1 + " It’s less then ")
         } else if (age_1 >= age_2 && age_1 < age_3) {
@@ -70,5 +70,5 @@ const checkAge = (age_1) => {
 checkAge('2')
 checkAge('five5')
 checkAge(28)
-checkAge(0)
+checkAge('0')
 checkAge('')
